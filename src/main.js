548e405bb176150d73a1a1359a21e2ba8f87ce5c
Sebastián Campos <pseba@outlook.com>
2026-07-28 00:59:03 -0400

    configuración admin

--- src/main.js
+++ src/main.js
@@ -1,1 +1,5 @@
+import content from '../content/home.json';
 import './style.css'
+
+document.querySelector('#hero-title').textContent = content.hero_title;
+document.querySelector('#hero-image').src = content.hero_image;
---
+++ vite.config.js
@@ -0,0 +1,3 @@
+export default {
+  base: '/boletin-mensual-barrio-san-manuel/',
+}
