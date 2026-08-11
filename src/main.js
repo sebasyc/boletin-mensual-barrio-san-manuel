a38204b46e92b4579771dd4b74a5b777606e7832
Sebastián Campos <pseba@outlook.com>
2026-08-11 12:04:23 -0400

    test content desgin

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
 import './style.css'
 import content from '../content/home.json';
+import { initParallax } from './sections/parallax'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import AOS from 'aos'
+
+initParallax()
+
+
+AOS.init({
+  easing: 'ease-out',
+  once: true,
+  offset: 100, 
+})
 
 document.querySelector('#site-title').textContent = content.site_title;
 document.querySelector('#site-subtitle').textContent = content.site_subtitle;
